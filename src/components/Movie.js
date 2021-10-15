bbb83c750d3328c6673511f0022eb413396f3072
ZumTheZazaKing <zahidi85543@gmail.com>
2021-10-15 15:47:17 +0800

    Quick fix

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,17 @@
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
+import Unavailable from '../images/unavailable.png';
+
 export function Movie(props){
     return (<div className="movie">
-        <img src={`https://image.tmdb.org/t/p/w1280${props.data.poster_path}`}/>
+        <LazyLoadImage 
+        src={`https://image.tmdb.org/t/p/w1280${props.data.poster_path}`}
+        alt={props.data.title}
+        effect="blur"
+        placeholderSrc={Unavailable}
+        className="img"
+        />
         <h2>{props.data.title}</h2>
     </div>)
 }
